c7feab1d7ca12f8ad61faccfd9df5829ee9386dc
Chedda98 <twa05157@gmail.com>
2021-09-13 14:15:42 +0900

    Fix: test

--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 export default class Game {
   constructor (p1, p2) {
     this.p1 = p1
-    this.p2 = p2
+    this.p2 = p2 //이 부분을 p2로 설정해줘야 한다!
     this.board = [[null, null, null], [null, null, null], [null, null, null]]
     this.player = Math.random() < 0.5 ? this.p1 : this.p2
     this.sym = 'X'
